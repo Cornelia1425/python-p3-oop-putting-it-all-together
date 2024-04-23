f84a92b397307477057661b904d55d38345421d9
yiranshu <shuyiran1425@hotmail.com>
2024-04-23 15:03:57 -0400

    bookshoe

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__(self, title:str, page_count):
+        self.title = title
+        self._page_count = page_count #why _here?
+
         
+    @property #getting the value
+    def page_count(self):
+        return self._page_count
+   
+    @page_count.setter
+    def page_count(self, value):
+        if type(value) != int:
+            # self.page_count = "not an integer"
+            print ("page_count must be an integer") 
+        else:
+            self._page_count = value
+
+
+    def turn_page(self):
+        print ("Flipping the page...wow, you read fast!")
+        # print ("Flipping the page...wow, you read fast!")
